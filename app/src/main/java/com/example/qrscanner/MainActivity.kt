8868536f98b6645e37747c7b41d14a2fbda8aca7
aliciane-pinault <aliciane.pinault@isen.yncrea.fr>
2024-02-20 10:33:51 +0100

    lein fonctionel si on outre passe la verif du auto signe (ok avec wylli a preciser dans la doc)

--- app/src/main/java/com/example/qrscanner/MainActivity.kt
+++ app/src/main/java/com/example/qrscanner/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.qrscanner
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -9,6 +10,15 @@ import com.google.zxing.integration.android.IntentIntegrator
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okio.IOException
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+import java.security.cert.CertificateFactory
+import java.security.KeyStore
+import javax.net.ssl.SSLContext
+import java.io.InputStream
+import java.security.cert.X509Certificate
+import java.util.concurrent.TimeUnit
+import javax.net.ssl.TrustManagerFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,13 +46,132 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+    private fun getUnsafeOkHttpClient_1(): OkHttpClient {
+        // Créez un TrustManager qui fait confiance au certificat CAs dans votre keystore
+        val certificateFactory = CertificateFactory.getInstance("X.509")
+        val inputStream: InputStream = resources.openRawResource(R.raw.moncertificat) // le sertifiact du serveur
+        val certificate = certificateFactory.generateCertificate(inputStream)
+        inputStream.close()
 
-    private fun sendTokenToServer(jwtToken: String) {
-        val client = OkHttpClient()
+        // Créez un keystore contenant notre certificat de confiance
+        val keyStore = KeyStore.getInstance(KeyStore.getDefaultType()).apply {
+            load(null, null)
+            setCertificateEntry("ca", certificate)
+        }
+
+        // Utilisez-le pour construire un X509TrustManager.
+        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm()).apply {
+            init(keyStore)
+        }
+        val trustManagers = trustManagerFactory.trustManagers
+        check(trustManagers.size == 1 && trustManagers[0] is X509TrustManager) { "X509TrustManager non trouvé" }
+        val trustManager = trustManagers[0] as X509TrustManager
+
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(null, arrayOf<TrustManager>(trustManager), null)
+
+        return OkHttpClient.Builder()
+            .sslSocketFactory(sslContext.socketFactory, trustManager)
+            .readTimeout(30, TimeUnit.SECONDS) // Augmente le timeout de lecture à 30 secondes
+            .writeTimeout(30, TimeUnit.SECONDS) // Augmente le timeout d'écriture à 30 secondes
+            .connectTimeout(30, TimeUnit.SECONDS) // Augmente le timeout de connexion à 30 secondes
+            .build()
+    }
+
+
+    private fun getUnsafeOkHttpClient_2(): OkHttpClient {
+        try {
+            // Créez un gestionnaire de confiance qui ne valide pas les chaînes de certificats
+            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+                override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+            })
+
+            // Installez le gestionnaire de confiance tout en gardant le SSLContext vide
+            val sslContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+
+            // Créez un OkHttpClient et installez le socketFactory avec le sslContext
+            return OkHttpClient.Builder()
+                .sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
+                .build()
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+
+    private fun getUnsafeOkHttpClient_3(): OkHttpClient {
+        try {
+            // Créez un gestionnaire de confiance qui ne valide pas les chaînes de certificats
+            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+                override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+            })
+
+            // Installez le gestionnaire de confiance tout en gardant le SSLContext vide
+            val sslContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+
+            // Créez un OkHttpClient et installez le socketFactory avec le sslContext
+            val builder = OkHttpClient.Builder()
+                .sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
+                // Ajoutez le HostnameVerifier personnalisé ici
+                .hostnameVerifier { _, _ -> true }
+
+            return builder.build()
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+
+
+
+
+    //passer outre les verif https
+    private fun getUnsafeOkHttpClient(): OkHttpClient {
+        try {
+            // Créez un TrustManager qui ne fait pas de vérification des certificats
+            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<java.security.cert.X509Certificate>, authType: String) {
+                    // Ne fait rien, accepte tout
+                }
+
+                override fun checkServerTrusted(chain: Array<java.security.cert.X509Certificate>, authType: String) {
+                    // Ne fait rien, accepte tout
+                }
+
+                override fun getAcceptedIssuers(): Array<java.security.cert.X509Certificate> {
+                    return arrayOf() // Tableau vide
+                }
+            })
+
+            // Installez le TrustManager tout en gardant le SSLContext vide
+            val sslContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+
+            // Créez un OkHttpClient et installez le socketFactory avec le sslContext
+            val builder = OkHttpClient.Builder()
+                .sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
+                .hostnameVerifier { _, _ -> true } // Accepte n'importe quel nom d'hôte
+
+            return builder.build()
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+
+
+
+
+    //envoyer le token du QRcode vers le serveur en format json
+    private fun sendTokenToServer_1(jwtToken: String) {
+        val client = getUnsafeOkHttpClient() // Utilisez le client configuré
         val mediaType = "application/json".toMediaTypeOrNull()
         val body = RequestBody.create(mediaType, "{\"token\":\"$jwtToken\"}")
         val request = Request.Builder()
-            .url("http://votre_serveur/validate-jwt") // TODO : Remplacez l'URL de notre serveur
+            .url("https://autopresence.isen.fr/api/tests")
             .post(body)
             .addHeader("content-type", "application/json")
             .build()
@@ -50,8 +179,9 @@ class MainActivity : ComponentActivity() {
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 // Gestion de l'échec de la requête (n'arrive pas a envoyer la requete au serveur)
+                Log.e("Error",e.localizedMessage)
                 runOnUiThread {
-                    Toast.makeText(applicationContext, "Erreur lors de la connexion au serveur", Toast.LENGTH_LONG).show()
+                    Toast.makeText(applicationContext, "Erreur lors de la connexion au serveur : " + e, Toast.LENGTH_LONG).show()
                 }
             }
 
@@ -64,6 +194,45 @@ class MainActivity : ComponentActivity() {
                     } else {
                         // Gestion de la réponse d'erreur pour un token JWt invalide (pas reconnue pas la bonne heure ...)
                         Toast.makeText(applicationContext, "JWT Invalide", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        })
+    }
+
+    //envoyer des donnees sur le serveur sous forme de de text
+    private fun sendTokenToServer(qrContent: String) {
+        val client = getUnsafeOkHttpClient() // Utilisez le client configuré pour passer outre les vérifications SSL
+        // Définissez le MediaType pour "text/plain" au lieu de "application/json"
+        val mediaType = "text/plain".toMediaTypeOrNull()
+        // Créez un RequestBody avec le contenu QR en format texte
+        val body = RequestBody.create(mediaType, qrContent)
+        // Construisez la requête en précisant le type de contenu "text/plain"
+        val request = Request.Builder()
+            .url("https://autopresence.isen.fr/api/tests")
+            .post(body)
+            .addHeader("content-type", "text/plain")
+            .build()
+
+        // Effectuez l'appel réseau de manière asynchrone
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Gestion de l'échec de la requête
+                Log.e("Error", e.localizedMessage)
+                runOnUiThread {
+                    Toast.makeText(applicationContext, "Erreur lors de la connexion au serveur : " + e, Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                runOnUiThread {
+                    if (response.isSuccessful) {
+                        val responseBody = response.body?.string()
+                        // Afficher une notification pour une réponse réussie
+                        Toast.makeText(applicationContext, "Réponse du serveur : $responseBody", Toast.LENGTH_LONG).show()
+                    } else {
+                        // Gestion de la réponse d'erreur
+                        Toast.makeText(applicationContext, "Erreur de réponse du serveur", Toast.LENGTH_LONG).show()
                     }
                 }
             }
